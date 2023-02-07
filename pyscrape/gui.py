ceaa3dcbbdf3466bd5b9db4522021bb9b2e42f00
KTB GLOBAL <124087716+KTBGlobal@users.noreply.github.com>
2023-02-07 13:58:08 -0600

    bug fixes
    Some error handling and UI improvements.

--- pyscrape/gui.py
+++ pyscrape/gui.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from bs4 import BeautifulSoup
 import requests 
 from csv import writer
+import threading
 
 #Create an instance of tkinter frame
 win = Tk()
@@ -11,19 +12,27 @@ win = Tk()
 win.geometry("900x500")
 #app name
 win.title("THOMASNET SCRAPER")
-
+win.configure(bg="grey")
+### BUILD IN LOOP PAUSE. 
 #Define a function to show a message
 base_url = 'https://www.thomasnet.com/'
+
+#store last URL
+last_url = ''
 def myclick():
     url = entry.get()
-    message= "Working on scraping."+ url
-    label= Label(frame, text= message, font= ('Times New Roman', 14, 'italic'))
+    last_url = url
+    message= "SCRAPED: "+ url
+    label= Label(frame, text= message, font= ('Times New Roman', 8)).grid(column=0, row=15, padx=10, pady=25)
     entry.delete(0, 'end')
-    label.pack(pady=30)
+    #label.pack(pady=30)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #get link URL, then parse link URL and extract data that way. 
-    lists = soup.find_all('div', class_="profile-card__data") #use underscore or it looks for python class.
+    try:
+        lists = soup.find_all('div', class_="profile-card__data") #use underscore or it looks for python class.
+    except:
+        print('Error getting lists')
     counter = 0
     with open('leads.csv', 'a', encoding='utf8', newline='') as f:
         thewriter = writer(f)
@@ -44,16 +53,22 @@ def myclick():
                 location_div = page_search.find('span', {'class':'copro-address-line'})
                 location_name = location_div.text.split()
                 city_name = location_name[0].replace(",", '')
-                state_name = location_name[1].replace(",", '')
+                try:
+                    state_name = location_name[1].replace(",", '')
+                except:
+                    state_name = ''
                 i = 0
                 while(len(state_name) > 2):
                     i+=1
                     state_name = location_name[i].replace(",", '')
                 contact_p = page_search.find('p', {'class':'phoneline'})
-                phone_span = contact_p.findChildren("span" , recursive=False)
+                phone_span = contact_p.findChildren("span" , recursive=False) 
                 phone_number = phone_span[1].text
-                website_link = company_h1.find('a').get("href")
-                last_name = company_name
+                try:
+                    website_link = company_h1.find('a').get("href")
+                except:
+                    website_link = ''
+                last_name = company_name 
                 lead_type = 'Unknown'
                 info = [company_name, commodity_name, city_name, state_name, phone_number, website_link, last_name, lead_type]
                 thewriter.writerow(info)
@@ -61,8 +76,12 @@ def myclick():
                 print(str(counter) + " grabbing " + str(company_name))
             except AttributeError:
                 print("Error with " + str(counter))
-                counter+=1
+            #wait 1 second between loops to avoid crashing webserver. 
+            #Event.wait(1)
 
+#LABEL ABOVE URL INPUT
+w = ttk.Label(text="ENTER URL", compound="top", foreground="white", background="black")
+w.pack()
 #Creates a Frame
 frame = LabelFrame(win, width= 700, height= 400, bd=5)
 frame.pack()
@@ -71,10 +90,15 @@ frame.pack_propagate(False)
 
 #Create an Entry widget in the Frame
 entry = ttk.Entry(frame, width= 300)
-entry.insert(INSERT, "URL")
+print("last url: " + str(last_url))
+entry.insert(INSERT, last_url)
 entry.pack()
 #Create a Button
-ttk.Button(win, text= "Click", command= myclick).pack(pady=20)
+ttk.Button(win, text="GET LEADS", command= myclick).pack(pady=20)
+
+threads = []
+parser = threading.Thread(target=myclick)
+threads.append(parser)
 win.mainloop()
 
 
